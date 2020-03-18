b2d3330ded5a2d424cb0d60db1c980e0ebbf4b91
Sonia Sieiro <soniasieirod@gmail.com>
2020-03-18 12:33:17 +0100

    primer commit, estructura y pantalla categorias

---
+++ Discourse_SoniaSieiro/Topics/TopicsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TopicsViewController.swift
+//  Discourse_SoniaSieiro
+//
+//  Created by Sonia Sieiro on 17/03/2020.
+//  Copyright © 2020 Sonia Sieiro. All rights reserved.
+//
+
+import UIKit
+
+class TopicsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBOutlet weak var categories: UILabel!
+    
+}
---
+++ Discourse_SoniaSieiro/Users/UsersViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Discourse_SoniaSieiro
+//
+//  Created by Sonia Sieiro on 17/03/2020.
+//  Copyright © 2020 Sonia Sieiro. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBOutlet weak var Users: UILabel!
+    
+}
+
